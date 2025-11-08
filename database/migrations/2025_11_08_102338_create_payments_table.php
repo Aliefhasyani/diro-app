6d4a110b472cdca4ba414f7e5d805195545c5354
Aliefhasyani <aliefhasyani05@gmail.com>
2025-11-08 18:25:16 +0800

    feat.fixed migrations

---
+++ database/migrations/2025_11_08_102338_create_payments_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained('reservations')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->enum('payment_status', ['pending', 'paid', 'failed'])->default('pending');
+            $table->string('stripe_session_id')->nullable();
+            $table->string('stripe_payment_intent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
